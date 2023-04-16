22959059c311be20fbaf210c05f10ff91d200ea4
Andrés Torres <camitorres1404@gmail.com>
2023-04-16 16:41:40 -0500

    Audio Player terminado

--- src/components/EscucharAudio/Audio.jsx
+++ src/components/EscucharAudio/Audio.jsx
@@ -43,55 +43,84 @@ function Audio(props) {
         setCurrentTime(audioRef.current.currentTime);
     };
 
+    const ActiveVolume = () => {
+
+        if (audioRef.current.volume === 0) {
+            audioRef.current.volume = 1
+            setVolume(1)
+        } else {
+            audioRef.current.volume = 0
+            setVolume(0)
+        }
+    }
+
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time % 60);
+        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    }
+
     return (
         <div className="audio-player">
-            <div className="audio-player__progress">
+            <div className="audio-progress">
                 <input
                     type="range"
-                    className="audio-player__slider"
+                    className="audio-progress-bar"
+                    min="0"
+                    max={Math.floor(duration) || 0}
                     value={currentTime}
-                    max={duration || 0}
                     onChange={handleProgressChange}
-                    aria-label="Barra de progreso del audio"
                 />
-                <div className="audio-player__sound-wave" />
+                <span className="audio-time">{formatTime(currentTime)} / {formatTime(duration)}</span>
             </div>
             <div className="audio-player__controls">
                 <button
                     className="audio-player__button"
                     onClick={handleBackward}
                     aria-label="Atrasar 10 segundos"
+                    title='Atrasar 10 segundos'
                 >
                     <i className="fa-solid fa-backward"></i>
                 </button>
                 <button
                     className="audio-player__button"
                     onClick={handlePlayPause}
-                    aria-label={isPlaying ? "Pausar" : "Reproducir"}
+                    aria-label={isPlaying ? "Reproducir" : "Pausar"}
+                    title={isPlaying ? "Reproducir" : "Pausar"}
                 >
-                    {isPlaying ? <i className="fa-solid fa-play"></i> : <i className="fa-solid fa-pause"></i>}
+                    {isPlaying ? <i className="fa-solid fa-pause"></i> : <i className="fa-solid fa-play"></i>}
                 </button>
                 <button
                     className="audio-player__button"
                     onClick={handleForward}
                     aria-label="Adelantar 10 segundos"
+                    title="Adelantar 10 segundos"
                 >
                     <i className="fa-solid fa-forward"></i>
                 </button>
             </div>
-            <div className="audio-player__volume">
-                <span className="audio-player__volume-icon">
-                    <i className="fa-solid fa-volume-high"></i>
-                </span>
+            <div className="audio-volume">
+                <button
+                    className="audio-button"
+                    onClick={ActiveVolume}
+                    title={volume === 0 ? "Activar sonido" : "Silenciar"}
+                >
+                    {volume > 0.5 ? (
+                        <i className="fas fa-volume-up"></i>
+                    ) : volume > 0 & volume < 0.5 ? (
+                        <i className="fas fa-volume-down"></i>
+                    ) : (
+                        <i className="fas fa-volume-mute"></i>
+                    )}
+                </button>
                 <input
                     type="range"
-                    className="audio-player__slider"
-                    min={0}
-                    max={1}
-                    step={0.1}
+                    className="audio-volume-bar"
+                    min="0"
+                    max="1"
+                    step="0.01"
                     value={volume}
                     onChange={handleVolumeChange}
-                    aria-label="Control de volumen del audio"
                 />
             </div>
             <audio
