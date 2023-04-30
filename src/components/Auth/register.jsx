f089f29f2d62885abcb8451cd71401b7f45dca27
EdwarZD1998 <edwarzd1998@gmail.com>
2023-04-29 20:19:12 -0500

    validaciones registro

--- src/components/Auth/register.jsx
+++ src/components/Auth/register.jsx
@@ -1,69 +1,107 @@
-import React from 'react';
-import "../../assets/css/Auth.css"
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+import '../../assets/css/Auth.css';
 
-class Register extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      confirmPassword: ''
-    };
-  }
+function Register() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
 
-  manejarCambioEmail = (evento) => {
-    this.setState({ email: evento.target.value });
-  }
+  const [errors, setErrors] = useState({});
 
-  manejarCambioPassword = (evento) => {
-    this.setState({ password: evento.target.value });
-  }
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+    setErrors({ ...errors, [event.target.name]: '' });
+  };
 
-  manejarCambioConfirmPassword = (evento) => {
-    this.setState({ confirmPassword: evento.target.value });
-  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-  manejarEnvioFormulario = (evento) => {
-    evento.preventDefault();
-    if (this.state.password !== this.state.confirmPassword) {
-      alert('La contraseña y la confirmación de contraseña deben ser iguales.');
+    if (!formData.email) {
+      setErrors({ ...errors, email: 'El correo electrónico es requerido' });
       return;
     }
-    fetch('http://ejemplo.com/api/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password
-      })
-    })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
-  }
 
-  render() {
-    return (
-      <div className="contentWrapper row align-items-center justify-content-center pt-4 pb-4 px-2 gap-4 h-100">
-        <div className="container">
-          <form className='justify-content-center form-container' onSubmit={this.manejarEnvioFormulario}>
-            <h1 className='text-center'>Registro</h1>
-            <input type="email" placeholder="Correo electrónico" value={this.state.email} onChange={this.manejarCambioEmail} />
-            <input type="password" placeholder="Contraseña" value={this.state.password} onChange={this.manejarCambioPassword} />
-            <input type="password" placeholder="Confirmar contraseña" value={this.state.confirmPassword} onChange={this.manejarCambioConfirmPassword} />
-            <div className='frame'>
-              <button className='custom-bttn bttn' type="submit"><span>Registrarse</span></button>
-            </div>
-            <div className="__container">
-              <hr />
-              <Link to="/login">Ya tengo cuenta</Link>
-            </div>
-          </form>
-        </div>
+    if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      setErrors({ ...errors, email: 'El correo electrónico no es válido' });
+      return;
+    }
+
+    if (!formData.password) {
+      setErrors({ ...errors, password: 'La contraseña es requerida' });
+      return;
+    }
+
+    if (formData.password.length < 6) {
+      setErrors({
+        ...errors,
+        password: 'La contraseña debe tener al menos 6 caracteres',
+      });
+      return;
+    }
+
+    if (!formData.confirmPassword) {
+      setErrors({
+        ...errors,
+        confirmPassword: 'La confirmación de la contraseña es requerida',
+      });
+      return;
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      setErrors({
+        ...errors,
+        confirmPassword: 'La contraseña y su confirmación no coinciden',
+      });
+      return;
+    }
+
+    const user = {
+      email: formData.email,
+      password: formData.password,
+    };
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_DJANGO_URL}/users/`, user);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="contentWrapper row align-items-center justify-content-center pt-4 pb-4 px-2 gap-4 h-100">
+      <div className="container">
+        <form className="justify-content-center form-container" onSubmit={handleSubmit}>
+          <h1 className="text-center">Crear una cuenta</h1>
+          <div className="grupo-input">
+            <input type="email" placeholder="Correo electrónico" name="email" value={formData.email} onChange={handleChange} />
+          </div>
+          {errors.email && <p className="error-message">{errors.email}</p>}
+          <div className="grupo-input">
+            <input type="password" placeholder="Contraseña" name="password" value={formData.password} onChange={handleChange} />
+          </div>
+          {errors.password && <p className="error-message">{errors.password}</p>}
+          <div className="grupo-input">
+            <input type="password" placeholder="Confirmar contraseña" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} />
+          </div>
+          {errors.confirmPassword && <p className="error-message">{errors.confirmPassword}</p>}
+          <div className='frame'>
+            <button type="submit" className='custom-bttn bttn'>
+              Crear cuenta
+            </button>
+          </div>
+          <div className="__container">
+            <hr />
+            <Link to="/login">Ya tengo cuenta</Link>
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Register;
